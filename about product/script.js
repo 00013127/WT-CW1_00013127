46436ef0d615c8a57713fadb95e2242d0e1d4ad9
Akbar <akbarboss91@gmail.com>
2022-11-21 22:10:58 +0500

    Structure change for Deploy

--- about product/script.js
+++
@@ -1,5 +0,0 @@
-let productName = localStorage.getItem("productName");
-let productPrice = localStorage.getItem("productPrice");
-
-document.getElementById("product-name").innerText = productName;
-document.getElementById("product-price").innerText = productPrice;
--- products/script.js
+++
@@ -1,9 +0,0 @@
-let btns = document.querySelectorAll(".product-button")
-
-for (let i = 0; i<btns.length; i++) {
-    btns[i].addEventListener("click", () => {
-        localStorage.setItem("productName", btns[i].previousElementSibling.innerText);
-        localStorage.setItem("productPrice", btns[i].previousElementSibling.getAttribute('data'));
-        window.location = "../about product/index.html";
-    })
-}
